55b85754875a6d3f55a98db6143edb249e34663b
Everton-Sena19 <Everton-Sena19>
2026-06-22 06:54:25 -0300

    feat: modal premium PWA e ajustes de cache

--- sw.js
+++ sw.js
@@ -1,46 +1,23 @@
-const CACHE_NAME = 'novo-visual-v2';
+const CACHE_NAME = 'novo-visual-v1';
 
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/manifest.json'
+
+  './',
+  './index.html',
+  './style.css',
+  './js/script.js',
+  './manifest.json'
+
 ];
 
 self.addEventListener('install', (event) => {
 
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
   );
-
-  self.skipWaiting();
-
-});
-
-self.addEventListener('activate', (event) => {
-
-  event.waitUntil(
-
-    caches.keys().then((cacheNames) => {
-
-      return Promise.all(
-
-        cacheNames.map((cacheName) => {
-
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-
-        })
-
-      );
-
-    })
-
-  );
-
-  self.clients.claim();
 
 });
 
@@ -50,7 +27,6 @@ self.addEventListener('fetch', (event) => {
 
     caches.match(event.request)
       .then((response) => {
-
         return response || fetch(event.request);
 
       })
